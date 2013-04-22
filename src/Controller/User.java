79ec29aac4cc8d5b3de740b6c35a3a432727fae2
Anupam <anupam.jindal@hp.com>
2013-04-21 22:07:48 -0700

    setting browser cookies during login/registration

--- src/Controller/User.java
+++ src/Controller/User.java
@@ -18,22 +18,22 @@ public class User {
 	{
 		DBUtil dbUtil = new DBUtil();
 		
-		Map reqParamMap = req.getParameterMap();
+		Map<String, String[]> reqParamMap = req.getParameterMap();
 		
 		BasicDBObject userDoc = new BasicDBObject();
 		BasicDBObject useraddrDoc = new BasicDBObject();
 		
-		useraddrDoc.put("street1", reqParamMap.get("street1"));
+		/*useraddrDoc.put("street1", reqParamMap.get("street1"));
 		useraddrDoc.put("street2", reqParamMap.get("street2"));
 		useraddrDoc.put("city", reqParamMap.get("city"));
 		useraddrDoc.put("state", reqParamMap.get("state"));
-		useraddrDoc.put("zip", reqParamMap.get("zip"));
+		useraddrDoc.put("zip", reqParamMap.get("zip"));*/
 		
-		userDoc.put("email", reqParamMap.get("email"));
-		userDoc.put("name", reqParamMap.get("name"));
-		userDoc.put("password", reqParamMap.get("password"));
-		userDoc.put("phone", reqParamMap.get("phone"));
-		userDoc.put("address", useraddrDoc);
+		userDoc.put("email", reqParamMap.get("email")[0]);
+		userDoc.put("name", reqParamMap.get("name")[0]);
+		userDoc.put("password", reqParamMap.get("password")[0]);
+		//userDoc.put("phone", reqParamMap.get("phone"));
+		//userDoc.put("address", useraddrDoc);
 		
 		String id = null;
 		try {
@@ -52,6 +52,22 @@ public class User {
 		//String userInfo = dbUtil.queryDocs(USER_COLLECTION, queryObj);
 		
 		return dbUtil.queryDocs(USER_COLLECTION, queryObj);
+	}
+	
+	public JSONObject verifyUserInfo(String email, String password) throws UnknownHostException
+	{
+		BasicDBObject queryObj = new BasicDBObject();
+		queryObj.put("email", email);
+		queryObj.put("password", password);
+		DBUtil dbUtil = new DBUtil();
+		JSONObject returnUser = dbUtil.queryDocs(USER_COLLECTION, queryObj);
+		
+		if(returnUser.isEmpty()) {
+			System.out.println("user Not Found");
+			return null;
+		}
+		System.out.println("user Found" + returnUser);
+		return returnUser;
 	}
 
 }
--- src/servlet/RegistrationServlet.java
+++ src/servlet/RegistrationServlet.java
@@ -6,6 +6,9 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import Controller.Event;
+import Controller.User;
+
 /**
  * Servlet implementation class RegistrationServlet
  */
@@ -31,7 +34,19 @@ public class RegistrationServlet extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		System.out.println("save new user");
+		
+		User newUser = new User();
+		String userId = newUser.saveUserInfo(request);
+		if(userId == null)
+		{
+			throw new ServletException("user registration failed");
+		}
+		String contextPath = request.getContextPath();
+		response.setStatus(HttpServletResponse.SC_OK);
+		response.setHeader("Set-Cookie", "socialfooduid=" + userId+ "; path=/Learn");
+		
+		response.sendRedirect(response.encodeRedirectURL(contextPath) + "/index.html");
 	}
 
 }
