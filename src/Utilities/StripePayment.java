dfe12b2d7f4b1b86c3716f5d871650b88a3c53a0
Anupam <anupam.jindal@hp.com>
2013-07-02 23:58:04 -0700

    stripe payment complete

--- src/Utilities/StripePayment.java
+++ src/Utilities/StripePayment.java
@@ -25,8 +25,8 @@ public class StripePayment {
 
 	final static String API_KEY = "sk_test_T8oA8Sl7loqlyKgeKlf3xCQG";
 	
-	static ArrayList<String> getStripeTokenList() throws UnknownHostException {
-		ArrayList<String> tokenList = new ArrayList<String>();
+	static Map<String, Double> getStripeTokenList() throws UnknownHostException {
+		Map<String, Double> tokenList = new HashMap<String, Double>();
 		
 		Event event = new Event();
 		JSONObject allEventsObj = event.getAllEvents();
@@ -35,6 +35,7 @@ public class StripePayment {
 		for(int i=0; i<allEvents.size(); i++) {
 			Map eachEvent = (Map)allEvents.get(i);
 			
+			Float pricePerSeat = Float.parseFloat(eachEvent.get("price").toString());
 			BasicDBList attendeeList = new BasicDBList();
 			if(eachEvent.containsKey("attendees")) {
 				System.out.println("registered attendees: " +eachEvent.get("attendees"));
@@ -43,17 +44,21 @@ public class StripePayment {
 			
 			for(int j=0;j<attendeeList.size();j++) {
 				BasicDBObject eachAttendee = (BasicDBObject) attendeeList.get(j);
-				tokenList.add(eachAttendee.getString("stripePayment"));
+				
+				//Amount
+				int totalGuests = Integer.parseInt(eachAttendee.getString("totalGuests"));
+				Double amount = new Double(totalGuests*pricePerSeat);
+				tokenList.put(eachAttendee.getString("stripePayment"),amount);
 			}
 		}
 		return tokenList;
 	}
 	
 	static void chargeWithStripe() throws UnknownHostException, AuthenticationException, InvalidRequestException, APIConnectionException, CardException, APIException {
-		ArrayList<String> stripeTokenList = getStripeTokenList();
+		Map<String, Double> stripeTokenList = getStripeTokenList();
 		Stripe.apiKey = "sk_test_T8oA8Sl7loqlyKgeKlf3xCQG";
-		
-		for(String eachToken: stripeTokenList) {
+				
+		for(String eachToken: stripeTokenList.keySet()) {
 				HashMap<String, Object> customerParams = new HashMap<String, Object>();
 				customerParams.put("card", eachToken);
 				customerParams.put("description", "socialFood payment");
@@ -62,8 +67,9 @@ public class StripePayment {
 
 				HashMap<String, Object> chargeParams = new HashMap<String, Object>();
 				chargeParams.put("currency", "usd");
-				chargeParams.put("amount", 800);
+				chargeParams.put("amount", stripeTokenList.get(eachToken)*100);
 				chargeParams.put("customer", customer.getId());
+				
 				Charge.create(chargeParams);
 				System.out.println("payment complete for " + eachToken);
 		}
